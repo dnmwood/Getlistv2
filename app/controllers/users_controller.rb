35cc55c335a123b60dafd5457a735a57cd0c43af
DAVID <davidnmwood@gmail.com>
2017-11-07 11:27:26 -0500

    setup basic user controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,35 @@
 class UsersController < ApplicationController
-  def login
-  end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path, notice: "You're signed up!"
+    else
+      render "new"
+      flash[:notice] = "Form is invalid"
+    end
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      flash[:error] = "Something went wrong."
+      redirect_to user_path
+    end
   end
 
   def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
