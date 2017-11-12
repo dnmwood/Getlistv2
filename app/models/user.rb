2a3c721a7adbd0b3eb91037c7a3b3e06acc824e3
DAVID <davidnmwood@gmail.com>
2017-11-11 19:40:49 -0500

    Further tests, password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
+  before_save { email.downcase! }
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 50 },
                     uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_many :setlists
   has_many :songs
