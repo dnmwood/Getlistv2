55b7a6c5b2599b41958dc0f9441085ad345e8b86
DAVID <davidnmwood@gmail.com>
2018-01-05 14:23:46 -0500

    setup create setlist option for user

--- app/controllers/setlists_controller.rb
+++ app/controllers/setlists_controller.rb
@@ -1,10 +1,27 @@
 class SetlistsController < ApplicationController
   def index
+    @setlists = Setlist.all
+  end
+
+  def create
+    @setlist = @user.setlists.build(setlist_params)
+
+    if @setlist.save
+      redirect_to user_setlist_path(@user, @setlist), notice: "Your setlist was created!"
+    else
+      render :new
+    end
   end
 
   def new
+    @setlist = Setlist.new
   end
 
   def show
+  end
+
+  private
+  def setlist_params
+    params.require(:setlist).permit(:title)
   end
 end
