b759fd55b58ee303fd0911263edd468173ff984f
DAVID <davidnmwood@gmail.com>
2018-01-05 14:21:30 -0500

    user_id setup for dashboard page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,16 @@
 class DashboardController < ApplicationController
+  before_action :authorize
+
   def index
+    if current_user
+      @user_id = current_user.id
+    end
   end
+
+  private
+
+  def authorize
+    redirect_to 'sessions#new' unless current_user
+  end
+
 end
