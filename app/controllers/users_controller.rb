d112683c140a1e72bd44775fea0a86852871f11f
DAVID <davidnmwood@gmail.com>
2018-01-05 14:23:16 -0500

    quick fix for redirecting user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_url, notice: "You're signed up!"
+      redirect_to '/dashboard', notice: "You're signed up!"
     else
       render "new"
       flash[:notice] = "Form is invalid"
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
   end
 
   private
