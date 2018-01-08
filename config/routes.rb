d38ce62d5ba3ba3fede7c9fe6ea3a222dc55e63c
DAVID <davidnmwood@gmail.com>
2018-01-07 23:58:04 -0500

    Reverse decision to nest resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root 'sessions#new'
 
-  resources :users do
-    resources :dashboard, only: [:index]
-    resources :setlists
-    resources :songs
-  end
+  resources :users
+  resources :dashboard, only: [:index]
+  resources :setlists
+  resources :songs
 
   get '/dashboard', to: 'dashboard#index'
   get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
 
   resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
