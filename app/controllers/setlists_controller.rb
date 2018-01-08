be8a1eab2a16b91067fb1a65f2aecafe0194b6a6
DAVID <davidnmwood@gmail.com>
2018-01-08 16:46:49 -0500

    Setlists & songs saving, need to work on user relationship with models

--- app/controllers/setlists_controller.rb
+++ app/controllers/setlists_controller.rb
@@ -1,23 +1,34 @@
 class SetlistsController < ApplicationController
+
   def index
     @setlists = Setlist.all
-  end
-
-  def create
-    @setlist = @user.setlists.build(setlist_params)
-
-    if @setlist.save
-      redirect_to user_setlist_path(@user, @setlist), notice: "Your setlist was created!"
-    else
-      render :new
-    end
   end
 
   def new
     @setlist = Setlist.new
   end
 
+  def create
+    @setlist = Setlist.new(setlist_params)
+
+    if @setlist.save
+      redirect_to setlists_path, notice: "Your setlist was created!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @setlist = Setlist.find(params[:id])
+    if @setlist.update_attributes(setlist_params)
+      redirect_to '/dashboard'
+    else
+      render :edit
+    end
+  end
+
   def show
+    @setlist = Setlist.find_by(params[:id])
   end
 
   private
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,35 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
   def new
+    @song = Song.new
   end
 
   def create
+    @song = Song.new(song_params)
+
+    if @song.save
+      redirect_to songs_url, notice: "You have created a song!"
+    else
+      redirect_to '/dashboard', alert: "Nadda!"
+    end
+  end
+
+  def show
+    @song = Song.find_by(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def song_params
+    params.require(:song).permit(:title)
   end
 end
