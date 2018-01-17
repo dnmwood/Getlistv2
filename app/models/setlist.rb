275f72f2d034f6fa2b7cf235806e0c5aad73cfba
DAVID <davidnmwood@gmail.com>
2018-01-16 22:01:51 -0500

    Songs belong to setlists

--- app/models/setlist.rb
+++ app/models/setlist.rb
@@ -1,5 +1,6 @@
 class Setlist < ApplicationRecord
   belongs_to :user
+  has_many :songs
 
   validates :title, length: { minimum: 1, message: "ERROR MESSAGE" }
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ApplicationRecord
   belongs_to :user
+  belongs_to :setlist
 
   validates :title, length: { minimum: 1, message: "ERROR MESSAGE" }
 end
