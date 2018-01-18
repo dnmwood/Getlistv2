9c184d520df6c392402d953aca8987b1faf8a821
DAVID <davidnmwood@gmail.com>
2018-01-17 23:47:11 -0500

    Remove association between song and setlist, might not need them

--- app/models/setlist.rb
+++ app/models/setlist.rb
@@ -1,6 +1,5 @@
 class Setlist < ApplicationRecord
   belongs_to :user
-  has_many :songs
 
   validates :title, length: { minimum: 1, message: "ERROR MESSAGE" }
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,5 @@
 class Song < ApplicationRecord
   belongs_to :user
-  belongs_to :setlist
 
   validates :title, length: { minimum: 1, message: "ERROR MESSAGE" }
 end
